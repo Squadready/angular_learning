4422ab96a7008b1aceae0bd491504759ec4023da
islamovrhino <islamov.rhinoda@gmail.com>
2020-07-27 15:11:31 +0600

    admin with authentication, auth guard and post creation

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCyQyCl7fMUpapMcv-_lRcgDbfTUT72ev0',
+  fbDbUrl: 'https://angular-blog-e7be6.firebaseio.com'
+};
